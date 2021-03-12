0def73524f8b4caa8581418252d25e3bb2a8faeb
Cesar Camillo <73132612+CeCamillo@users.noreply.github.com>
2021-03-11 23:06:19 -0300

    Uri pexercises
    Exercises from 1000 to 1010

---
+++ 1005.py
@@ -0,0 +1,5 @@
+N1 = float(input())
+N2 = float(input())
+MEDIA = (N1 * 3.5 + N2 * 7.5)/11
+
+print('MEDIA = {:.5f}'.format(MEDIA))
---
+++ 1010.py
@@ -0,0 +1,9 @@
+linha1 = 
+
+cod_peca =
+num_pecas =
+valor =
+
+cod_peca2 =
+num_pecas2 =
+valor2 =
