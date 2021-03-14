5b39d8d7071d52cea43faeaa8f6e831a6a1de84a
Cesar Camillo <cesarcamillo2212@gmail.com>
2021-03-14 14:14:52 -0300

    Update 1010.py
    1010 solved

--- 1010.py
+++ 1010.py
@@ -1,9 +1,14 @@
-linha1 = 
+roupas1 = input().split()
 
-cod_peca =
-num_pecas =
-valor =
+c1 = int(roupas1[0])
+n1 = int(roupas1[1])
+v1 = float(roupas1[2])
 
-cod_peca2 =
-num_pecas2 =
-valor2 =
+roupas2 = input().split()
+
+c2 = int(roupas2[0])
+n2 = int(roupas2[1])
+v2 = float(roupas2[2])
+
+total = (n1 * v1) + (n2 * v2)
+print('VALOR A PAGAR: R$ {:.2f}'.format(total))
