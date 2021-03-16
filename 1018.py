c6f62c4e9b0a6c3b67af5a5225ab13208a2390c3
Cesar Camillo <cesarcamillo2212@gmail.com>
2021-03-16 16:22:09 -0300

    Uri 1018 - 1019

--- 1018.py
+++ 1018.py
@@ -1,12 +1,31 @@
-valor = int(input('escreva'))
+valor = int(input())
 
-nota100 = 100
-nota50 = 50
-nota20 = 20
-nota10 = 10
-nota5 = 5
-nota2 = 2
-nota1 = 1
+n100 = valor // 100
+r = valor % 100
 
+n50 = r // 50
+r = r % 50
 
+n20 = r // 20
+r = r % 20
 
+n10 = r // 10
+r = r % 10
+
+n5 = r // 5
+r = r % 5
+
+n2 = r // 2
+r = r % 2
+
+n1 = r // 1
+r = r % 1
+
+print(valor)
+print('{} nota(s) de R$ 100,00'.format(n100))
+print('{} nota(s) de R$ 50,00'.format(n50))
+print('{} nota(s) de R$ 20,00'.format(n20))
+print('{} nota(s) de R$ 10,00'.format(n10))
+print('{} nota(s) de R$ 5,00'.format(n5))
+print('{} nota(s) de R$ 2,00'.format(n2))
+print('{} nota(s) de R$ 1,00'.format(n1))
---
+++ 1019.py
@@ -0,0 +1,8 @@
+
+tempo = int(input())
+
+horas = tempo // 3600
+minutos =  (tempo % 3600) // 60
+segundos =  tempo % 60
+
+print('{}:{}:{}'.format(horas, minutos, segundos))
